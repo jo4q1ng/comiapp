03496c75b3f8ac76511f0ddeaa3164a31ddde09f
jo4q1ng <jo.guerrabustamante@gmail.com>
2026-04-06 23:43:53 -0400

    fix: service worker v3 con skipWaiting y clients.claim

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
-const CACHE = 'comiapp-v2';
-const ARCHIVOS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/config.js'];
+const CACHE = 'comiapp-v3';
+const ARCHIVOS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/config.js',
+  '/sw-register.js'
+];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
