64cb4469d519bd9b5fcc7ef77d3f19ba028dcac5
jo4q1ng <jo.guerrabustamante@gmail.com>
2026-04-06 23:36:28 -0400

    fix: agregar config.js al service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'comiapp-v1';
-const ARCHIVOS = ['/', '/index.html', '/css/style.css', '/js/app.js'];
+const CACHE = 'comiapp-v2';
+const ARCHIVOS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/config.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
